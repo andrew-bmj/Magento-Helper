2b101805a7369f9c9f0258f0e2ce8cf27db2600e
AGlynfordBrown <aglynfordbrown@bmj.com>
2015-01-12 14:06:42 +0000

    initial commit

---
+++ MagentoHelper.Templates/TemplateHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagentoHelper.Templates
+{
+
+    public enum TemplateResolver
+    {
+        COMPANYNAME = 0,
+        MODULENAME = 1
+    }
+
+    public static class TemplateHelper
+    {
+
+    }
+}
---
+++ MagentoHelper.Templates/TemplatesService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagentoHelper.Templates
+{
+    public class TemplatesService
+    {
+        private string _moduleName;
+        private string _companyName;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="moduleName"></param>
+        /// <param name="companyName"></param>
+        public TemplatesService(string moduleName, string companyName)
+        {
+            this._moduleName = moduleName;
+            //BMJ by default
+            this._companyName = companyName;
+        }
+
+        /// <summary>
+        /// Replaces the blank tags in a template with values
+        /// </summary>
+        /// <param name="template"></param>
+        /// <returns>formatted template</returns>
+        public string PopulateTemplate(string template) {
+            if (template.Contains(@"{1}"))
+                return string.Format(template, this._companyName, this._moduleName);
+            return string.Format(template, this._moduleName);
+        }
+    }
+}
